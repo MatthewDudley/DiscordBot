445f9a6e86aa50a0fe3809e929012d4e313bac94
Matthew Dudley <md05062@georgiasouthern.edu>
2019-06-16 21:10:42 -0400

    minor updates bot commands

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DiscordBot
+
+## TODO:
+- [ ] Be able to change command prefix
+- [ ] Integrate with MLB api for game scores, stats, player info, etc
--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,49 @@
+# --- imports ---
 import discord
-from discord.ext import commands
 from config import BOT_TOKEN
+from discord.ext import commands, tasks
+from itertools import cycle
+# --- imports end ---
 
-client = commands.Bot(command_prefix = '.')
+# --- variables ---
+bot = commands.Bot(command_prefix = '.')
+# --- variables end ---
 
-@client.event
+# --- bot envents ---
+@bot.event
 async def on_ready():
-    print('Bot is ready...')
+    # set bot status and activity (Playing game)
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('.bot for commands'))
+    # log bot is running
+    print(f'{bot.name} is ready...')
+    
+# --- bot events end ---
 
-client.run(BOT_TOKEN)
+# --- bot commands ---
+@bot.command()
+async def ping(ctx):
+    print(f'Pong! {round(bot.latency * 1000)}ms')
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+# dev | stop running the bot
+@bot.command()
+async def stop(ctx):
+    print('Bot is going offline')
+    await bot.logout()
+# --- bot commands end ---
+
+
+# --- troll ---
+
+# --- troll end ---
+
+    
+
+
+# --- run bot ---
+try:
+    bot.run(BOT_TOKEN)
+except:
+    print('[!]ERROR - bot.run')
+    sys.exit(-1)
+# --- run bot end ---
