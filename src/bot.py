180344e910c5799c9d2a3e05e0559b23ac2e6e58
Matthew Dudley <md05062@georgiasouthern.edu>
2019-06-16 22:11:04 -0400

    updated base bot code for changing prefix

--- src/bot.py
+++ src/bot.py
@@ -10,25 +10,38 @@ bot = commands.Bot(command_prefix = '.')
 # --- variables end ---
 
 # --- bot envents ---
+## on_ready()
 @bot.event
 async def on_ready():
     # set bot status and activity (Playing game)
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('.bot for commands'))
     # log bot is running
-    print(f'{bot.name} is ready...')
+    print(f'{bot.user.name} is ready...')
     
 # --- bot events end ---
 
 # --- bot commands ---
+## help command
+#########################################################################S
+
+## ping command
 @bot.command()
 async def ping(ctx):
     print(f'Pong! {round(bot.latency * 1000)}ms')
     await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
+## change prefix command
+@bot.command()
+async def prefix(ctx, newPrefix):
+    prevPrefix = bot.command_prefix
+    print(f'Changing prefix command from {prevPrefix} to {newPrefix}')
+    bot.command_prefix = newPrefix
+    await ctx.send(f'New command prefix is {bot.command_prefix}!')
+
 # dev | stop running the bot
 @bot.command()
 async def stop(ctx):
-    print('Bot is going offline')
+    print(f'Bot is going offline')
     await bot.logout()
 # --- bot commands end ---
 
@@ -45,5 +58,4 @@ try:
     bot.run(BOT_TOKEN)
 except:
     print('[!]ERROR - bot.run')
-    sys.exit(-1)
 # --- run bot end ---
